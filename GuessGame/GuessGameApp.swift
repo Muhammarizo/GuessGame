3d99b8f13bc612ca9a80365ba428e81ee300e7dd
Muhammadrizo <muhammadrizo2026@gmail.com>
2025-04-12 10:51:42 +0500

    Initial Commit

---
+++ GuessGame/GuessGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessGameApp.swift
+//  GuessGame
+//
+//  Created by MacBookPro on 12.04.2025.
+//
+
+import SwiftUI
+
+@main
+struct GuessGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
